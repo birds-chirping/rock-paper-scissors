d2177468f75074263ec8d797b63d6129b86b51e6
Birds Chirping <afara.ninge22@gmail.com>
2022-10-14 20:19:49 +0300

    Add playRound(). Change strings case (from upper to titlecase)

--- rps.js
+++ rps.js
@@ -1,11 +1,17 @@
-const choices = ['rock', 'paper', 'scissors'];
+const choices = ['Rock', 'Paper', 'Scissors'];
 console.log(choices);
+
+
+// Convert string to title case
+function toTitleCase(str) {
+    return str[0].toUpperCase() + str.substring(1).toLowerCase();
+} 
+
 
 // Get computer's choice
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
-console.log(getComputerChoice());
 
 
 // Get player's choice
@@ -13,24 +19,39 @@ function getPlayerChoice() {
     // get input only if valid & convert to lowercase
     let choice;
     while (!choices.includes(choice)) {
-        choice = prompt("Rock, paper, scissors? ").toLowerCase();
+        choice = toTitleCase(prompt("Rock, paper, scissors? "));
     }
     return choice;
 }
-console.log("Player: " + getPlayerChoice());
+
 
 
 // Play round
 function playRound(playerSelection, computerSelection) {
-    // code
-    // print message
-    // return winner
+    switch(true) {
+        case (playerSelection === computerSelection):
+            return ("It's a tie!");
+        case (
+            playerSelection === 'Rock' && computerSelection === 'Scissors'
+            || playerSelection === 'Scissors' && computerSelection === 'Paper' ||
+            playerSelection === 'Paper' && computerSelection === 'Rock'
+            ):
+            return `You win! ${playerSelection} beats ${computerSelection}.`;
+        default:
+            return `You lose!  ${computerSelection} beats ${playerSelection}.`;
+    }
 }
 
 // Play game (5 rounds)
 function playGame() {
     // code
+    // for (let i = 0; i < 5; i++)
+    let winner = playRound(getPlayerChoice(), getComputerChoice());
+    alert(winner);
+    console.log(winner);
     // keep score
     // return winner
 }
+
+playGame();
 
