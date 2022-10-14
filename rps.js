ac4bce39f8065e7c5f42946a4613a02b4c6ba9d0
Birds Chirping <afara.ninge22@gmail.com>
2022-10-14 16:07:28 +0300

    Design algorithm. Add getComputerChoice function.

---
+++ rps.js
@@ -0,0 +1,29 @@
+const choices = ['rock', 'paper', 'scissors'];
+console.log(choices);
+
+// Get computer's choice
+function getComputerChoice() {
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+console.log(getComputerChoice());
+
+// Get player's choice
+function getPlayerChoice() {
+    // get input
+    // return choice
+}
+
+// Play round
+function playRound(playerSelection, computerSelection) {
+    // code
+    // print message
+    // return winner
+}
+
+// Play game (5 rounds)
+function playGame() {
+    // code
+    // keep score
+    // return winner
+}
+
