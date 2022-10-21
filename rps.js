8ac1edefc81b731177fc41b3aad24f5bd88be8d3
Birds Chirping <afara.ninge22@gmail.com>
2022-10-21 19:53:51 +0300

    Fix bug (score).
    Bug: score not updating after first 5 winning rounds. Fixed.

--- rps.js
+++ rps.js
@@ -1,59 +1,16 @@
-// Play game (5 rounds)
-// function playGGame() {
-//     let playerScore = 0;
-//     let computerScore = 0;
-//     let tie = 0;
-//     let cancel = 0;
-
-//     for (let i = 0; i < 5; i++) {
-//         let computerChoice = getComputerChoice(allChoices);
-//         let playerChoice = getPlayerChoice();
-//         if (playerChoice === null) {
-//             cancel = 1;
-//             break;
-//         }
-//         let winner = playGame(playerChoice, computerChoice);
-
-//         // Keep score
-//         winner === 'player' ? playerScore++ : winner === 'computer' ? computerScore++ : tie++;
-//         console.log(`Player: ${playerScore}, Computer: ${computerScore}, Tie: ${tie}`);
-//     }
-//     // Announce winner
-//     if (cancel === 0) {
-//         console.log(playerScore === computerScore ? 'Tie!' : playerScore > computerScore ? 'Player wins!' : 'Computer wins!');
-//     } else {
-//         console.log('The game was canceled.');
-//     }
-// }
-
 const allChoices = ['rock', 'paper', 'scissors'];
 const playerScore = document.querySelector('#player-score');
 const compScore = document.querySelector('#comp-score');
 const board = document.querySelector('.board');
-
-
 const choicesPics = document.querySelector('.graphic');
 const playerChoiceBoard = document.querySelector('.player-choice');
 const compChoiceBoard = document.querySelector('.comp-choice');
-
-
 const endScreen = document.querySelector('.at-end');
 const endMessage = document.querySelector('.end-message');
 const newGame = document.querySelector('#new-btn');
 const playerButtons = document.querySelectorAll('.player-btn');
 let playerChoice;
 let compChoice;
-
-
-
-// Convert string to sentence case.
-function toSentenceCase(str) {
-    if (str) {
-        return str[0].toUpperCase() + str.substring(1).toLowerCase();
-    } else {
-        return str;
-    }
-} 
 
 
 // Get computer's choice
@@ -90,6 +47,13 @@ function updateScore(winner) {
 }
 
 
+function resetScore () {
+    playerScore.textContent = '0';
+    compScore.textContent = '0';
+    newGame.removeEventListener('click', resetScore);
+}
+
+
 function getMessage(playerChoice, compChoice, winner) {
     let message;
     playerChoice = playerChoice.toUpperCase();
@@ -113,7 +77,6 @@ function playGame() {
     compChoice = getComputerChoice(allChoices);
     const winner = getWinner(playerChoice, compChoice);
 
-    // choicesPics.textContent = `${this.id}`;
     playerChoiceBoard.textContent = `${this.id}`.toUpperCase();
     compChoiceBoard.textContent = `${compChoice}`.toUpperCase();
 
@@ -121,17 +84,14 @@ function playGame() {
         choicesPics.classList.add('blur');      // Add blur on board
         updateScore(winner);
 
-        // Play 5 winning rounds
+        // Reset score after 5 winning rounds
         if (playerScore.textContent < 5 && compScore.textContent < 5) {
             endMessage.textContent = getMessage(playerChoice, compChoice, winner);
         } else {
             playerScore.textContent > compScore.textContent ? 
-            endMessage.textContent = 'Player WON!':
-            endMessage.textContent = 'Alien WON!';
-            newGame.addEventListener('click', () => {
-                    playerScore.textContent = '0';
-                    compScore.textContent = '0';
-                });  
+            endMessage.textContent = 'Human WON!':
+            endMessage.textContent = 'Rabbit WON!';
+            newGame.addEventListener('click', resetScore);  
         }
         endScreen.classList.add('visible');
     }, 300);
@@ -142,7 +102,6 @@ function startNewGame () {
     // Remove blur, clear board
     playerButtons.forEach(choice => {choice.classList.remove('onclick')});
     choicesPics.classList.remove('blur');
-    // choicesPics.textContent = ``;
     playerChoiceBoard.textContent = '';
     compChoiceBoard.textContent = '';
     endScreen.classList.remove('visible');
